6a61a5dddab296b718cacaff10a0c5f3e49c2d32
Joseph Kabemba <you@example.com>
2021-05-15 13:46:19 +0200

    feat: use reference with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { TextField } from "./TextField"
+import { TextField } from "./TextField";
 
-const App: React.FC =() =>{
+const App: React.FC = () => {
   return (
     <div>
       <h1>Joseph Madras de Luluabourg</h1>
-      <TextField text="hello" person={ {firstName: '', lastName: ''}}/>
+      <TextField
+        text="hello"
+        person={{ firstName: "", lastName: "" }}
+        handleChange={(e) => {
+          e.preventDefault();
+        }}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,23 +1,35 @@
+import { useRef, useState } from "react";
+
 interface Person {
   firstName: string;
   lastName: string;
 }
 
-
-interface Props{
+interface Props {
   text: string;
   ok?: boolean;
   i?: number;
   fn?: (bob: string) => string;
   person: Person;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const TextField: React.FC<Props> = () => {
+interface TextNode {
+  text: string;
+}
+
+const TextField: React.FC<Props> = ({ handleChange }) => {
+  // const [count, setCount] = useState<number | null | undefined>(5);
+  const [count, setCount] = useState<TextNode>({ text: "Top" });
+  // setCount({ text: "Trop coool" });
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div>
-      <input/>
+    <div ref={divRef}>
+      <input ref={inputRef} onChange={handleChange} />
     </div>
-  )
-}
+  );
+};
 
-export {TextField}
+export { TextField };
